483fca5976ac22e376a50efe518a8315a45e2116
fabrice641 <166943086+fabrice641@users.noreply.github.com>
2024-04-28 18:08:12 +0200

    Add files via upload

---
+++ open.php
@@ -0,0 +1,47 @@
+<?php
+
+try{
+
+class database{
+
+
+
+    private static $server ='';
+    private static $password = '';
+    private static $utente = '';
+    private static $database = '';
+
+
+
+
+public function __construct() {
+    
+
+  
+}
+
+
+
+$sql = 'SELECT id  FROM tablename WHERE 1';
+
+
+echo row[];
+
+
+$open = new mysqli("$server", "$password,", "$utente", "$database");
+echo $open -> database();
+
+
+}catch(Exception $e){
+
+die();
+
+
+}
+
+
+
+}
+
+
+
